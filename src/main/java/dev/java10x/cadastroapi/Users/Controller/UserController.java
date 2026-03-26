9d87c4696d13b2b69e911b3167e79b49b7a64d1a
Diego Corte <emptyyyxd@gmail.com>
2026-03-26 14:37:32 -0300

    feat: add user management endpoints and update migration for rank column

--- src/main/java/dev/java10x/cadastroapi/Users/Controller/UserController.java
+++ src/main/java/dev/java10x/cadastroapi/Users/Controller/UserController.java
@@ -1,8 +1,6 @@
 package dev.java10x.cadastroapi.Users.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -12,5 +10,43 @@ public class UserController {
     public String welcome(){
         return  "this is the first message of this route";
     }
+
+    //Add user (Create)
+
+    @PostMapping("/register")
+    public String createUser(){
+        return "User successfully created";
+    }
+
+    //Show all Users (Read)
+
+    @GetMapping("/all")
+    public String showAllUsers(){
+        return "Show all users";
+    }
+
+    //Sort User by ID (Read)
+
+    @GetMapping("/showuserbyid")
+    public String showUserByID(){
+        return "Show user by ID";
+    }
+
+    //Update User data (Update)
+
+    @PutMapping("/update")
+    public String updateUserByID(){
+        return "Update user by ID";
+    }
+
+    //Delete User (Delete)
+
+    @DeleteMapping("/deletebyid")
+    public String deleteUserByID(){
+        return  "User deleted by id";
+    }
+
+
+
 
 }
--- src/main/resources/db/migrations/V2__Add_rank_tb_users.sql
+++ src/main/resources/db/migrations/V2__Add_rank_tb_users.sql
@@ -1,4 +1,3 @@
 -- Migrations to add rank column to the users table
--- Migrations table is immutable
 ALTER TABLE tb_users
 ADD COLUMN rank VARCHAR(255);
